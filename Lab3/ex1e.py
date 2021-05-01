49a1646b3c2dae5cbe086841931b68bead580acd
Viraj Dhanushka <59219626+viradhanus@users.noreply.github.com>
2021-05-01 09:39:22 +0530

    Update ex1e.py

--- Lab3/ex1e.py
+++ Lab3/ex1e.py
@@ -5,7 +5,7 @@ url = 'https://api.github.com/user/repos'
 
 with requests.Session() as session:
 
-    session.headers['Authorization'] = 'token 5ffaf0a4fcea7fd13d897967707e390a6947a267'
+    session.headers['Authorization'] = 'token '
 
     data = json.dumps({'name':'test', 'description':'some test repo'})
     response = session.post(url,data)
