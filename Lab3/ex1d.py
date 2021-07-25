7233ad3efe59286064239af2e2421e0f6e502d8c
Viraj Dhanushka <59219626+viradhanus@users.noreply.github.com>
2021-07-25 11:46:16 +0530

    Update ex1d.py

--- Lab3/ex1d.py
+++ Lab3/ex1d.py
@@ -2,7 +2,7 @@ import requests
 
 with requests.Session() as session:
 
-    session.headers['Authorization'] = 'token 5ffaf0a4fcea7fd13d897967707e390a6947a267'
+    session.headers['Authorization'] = 'token '
     response = session.get('https://api.github.com/user')
 
 print(response.headers)
